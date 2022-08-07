b81414af1e8a151355274b4d7ca89b79a646accd
Mayonezyck <yicheng_zhu@outlook.com>
2022-08-07 17:03:27 -0400

    remove unused and add two svgs

--- compare.m
+++ compare.m
@@ -4,16 +4,18 @@
  ans_path = 'ans/';
  files = dir(strcat(ans_path,'*.tif'));
  result_IOU= [];
+ result_Dice= [];
  for img = files'
    ansname = strcat(ans_path,img.name);
-   prename = strcat(prediction_path,string(extractBetween(img.name,'sawdust','.tif')),'_predict.png');
+   prename = strcat(prediction_path,string(extractBetween(img.name,'marble','.tif')),'_predict.png');
    a=imread(ansname);
    b=imread(prename);
    a=a(:,:,1);
    b=b(:,:,1);
    a=imresize(a,[256,256]);
    b=imresize(b,[256,256]);
-   a=imbinarize(a,0.5);
+   %comment this following line when comparing to an original label
+   %a=imbinarize(a,0.5);
    b=imbinarize(b,0.5);
    intersection=((a==b)&(a==0));
    a=-1*a+1;
@@ -21,6 +23,11 @@
    count=sum(intersection(intersection==1));
    union=sum(a(a==1))+sum(b(b==1))-count;
    IOU_value = count/union;
+   sumTwo = sum(a(a==1)) + sum(b(b==1));
+   Dice_value = 2*count/sumTwo;
    result_IOU = [result_IOU,IOU_value];
+   result_Dice = [result_Dice,Dice_value];
+
  end
- writematrix(result_IOU,'/home/zyck/Downloads/unet-test/unet-master/result.xlsx','Sheet',1,'Range','I47')
+ %writematrix(result_IOU,'/home/zyck/Downloads/unet-test/unet-master/result.xlsx','Sheet',1,'Range','I47')
+ writematrix(result_Dice,'/home/zyck/Downloads/unet-test/unet-master/result.xlsx','Sheet',1,'Range','I52')
--- evaluation.m
+++ evaluation.m
@@ -4,6 +4,7 @@
  myDir = '/home/zyck/Downloads/unet-test/unet-master/Kfolder/total/ans'; %gets directory
  myFiles = dir(fullfile(myDir,'*.tif')); %gets all tif files in struct
  result_IOU= [];
+ result_Dice= [];
  for k = 1:length(myFiles)
    baseFileName = myFiles(k).name;
    iname=extractBefore(baseFileName,'.tif');
@@ -18,7 +19,11 @@
    b=-1*b+1;
    count=sum(intersection(intersection==1));
    union=sum(a(a==1))+sum(b(b==1))-count;
+   sumTwo = sum(a(a==1)) + sum(b(b==1));
+   Dice_value = 2*count/sumTwo;
    IOU_value = count/union;
    result_IOU = [result_IOU,IOU_value];
+   result_Dice = [result_Dice,Dice_value];
  end
- writematrix(result_IOU,'/home/zyck/Downloads/unet-test/unet-master/result.xlsx','Sheet',1,'Range','I21')
+ %writematrix(result_IOU,'/home/zyck/Downloads/unet-test/unet-master/result.xlsx','Sheet',1,'Range','I21')
+ %writematrix(result_Dice,'/home/zyck/Downloads/unet-test/unet-master/result.xlsx','Sheet',1,'Range','I58')
